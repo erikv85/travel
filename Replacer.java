3d0878ad0fc541cbdce8869bf0fbd89a77fda2b5
Erik Vesterlund <erik.vesterlund@gmail.com>
2018-10-10 23:09:34 +0200

    matcher-independent backrefs already exist

--- Replacer.java
+++ Replacer.java
@@ -2,35 +2,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Replacer {
-	public static String replacer2(String line, String regex, String replacement) {
-		Pattern p = Pattern.compile(regex);
-		Matcher m = p.matcher(line);
-		StringBuffer sb = new StringBuffer();
-		while (m.find()) {
-			m.appendReplacement(sb, replacement);
-		}
-		m.appendTail(sb);
-		return sb.toString();
-	}
-
 	public static void main(String[] args) {
-		final String str = "erik";
-		final String key1 = "hey " + str;
-		final String key2 = "hey \\$1";
-		String res = replacer2(str, "(\\S+)", "hey $1");
-		if (key1.equals(res)) {
-			println("correct. "+str+" was replaced by "+res);
-		} else {
-			println("inccorect. "+str+" should have been replaced by "
-					+ key1 +" but was instead replaced by '"+res+"'");
-		}
-		res = replacer2(str, "(\\S+)", "hey \\$1");
-		if (key2.equals(res)) {
-			println("correct. "+str+" was replaced by "+res);
-		} else {
-			println("inccorect. "+str+" should have been replaced by "
-					+ key2 +" but was instead replaced by '"+res+"'");
-		}
+		final String in = "key1 val1 key2 \"val 2\"";
+		String re = "(\\S+)\\s+([^\\s\"]+|\"[^\"]*\")";
+		println(in.replaceAll(re, "$1 $2,"));
 	}
 
 	private static void println(Object o) {
