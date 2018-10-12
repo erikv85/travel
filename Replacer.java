74ad3c664d7b9b728fa4227cc4b1fe5c503e660e
Erik Vesterlund <erik.vesterlund@gmail.com>
2018-10-12 16:18:43 -0400

    indent 4 spaces

--- Replacer.java
+++ Replacer.java
@@ -1,11 +1,11 @@
 public class Replacer {
-	public static void main(String[] args) {
-		final String in = "key1 val1 key2 \"val 2\"";
-		String re = "(\\S+)\\s+([^\\s\"]+|\"[^\"]*\")";
-		println(in.replaceAll(re, "$1 $2,"));
-	}
+    public static void main(String[] args) {
+        final String in = "key1 val1 key2 \"val 2\"";
+        String re = "(\\S+)\\s+([^\\s\"]+|\"[^\"]*\")";
+        println(in.replaceAll(re, "$1 $2,"));
+    }
 
-	private static void println(Object o) {
-		System.out.println(o);
-	}
+    private static void println(Object o) {
+        System.out.println(o);
+    }
 }
